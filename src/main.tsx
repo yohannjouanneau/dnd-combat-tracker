fd599aae24d6a7a6db9bad4cfc355074e6667c15
Yohann <yohann.jouanneau@gmail.com>
2025-12-14 23:29:18 -0500

    move context and provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n";
 import App from "./App.tsx";
-import { ConfirmationDialogProvider } from "./hooks/ConfirmationDialogProvider.tsx";
-import { ToastProvider } from "./components/common/Toast/ToastProvider.tsx";
+import { ConfirmationDialogProvider } from "./contexts/ConfirmationDialogProvider.tsx";
+import { ToastProvider } from "./contexts/ToastProvider.tsx";
 import { ThemeProvider } from "./contexts/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
